e762898519623752c3e17cd9b7fc7aeae0e92091
M006004 <76555348+M006004@users.noreply.github.com>
2021-11-27 21:11:09 +0900

    setting weather

---
+++ src/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "196fc518884742e7910c722bbb8d8bd9";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const city = document.querySelector(".city");
+            city.innerText = data.name;
+
+            const weather = document.querySelector(".weather");
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
+}
+
+function onGeoError() {
+    alert("Can't find you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
