dd03f9160cfb611aab43f34b565f616f289ed403
M006004 <76555348+M006004@users.noreply.github.com>
2021-11-28 03:34:41 +0900

    calender setting

--- src/weather.js
+++ src/weather.js
@@ -2,13 +2,17 @@ const API_KEY = "196fc518884742e7910c722bbb8d8bd9";
 
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
+    const lon = position.coords.longitude;;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            const weather = document.querySelector(".weather");
-            weather.innerText = `${data.weather[0].main}`;
+            const weatherImg = document.querySelector(".weatherIcon");
+                 weatherImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+
+                 console.log(weatherImg);
+
+                 document.querySelector("#json > ul > li:nth-child(2) > div > ul > li > div > ul > li:nth-child(2) > div > span.type-string")
 
             const temp = document.querySelector(".temp");
             temp.innerText = `${data.main.temp}°C`;
