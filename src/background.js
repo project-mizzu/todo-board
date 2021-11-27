f0f3708584e3e58ed542ef969ed1f99111af34ee
M006004 <76555348+M006004@users.noreply.github.com>
2021-11-27 20:44:46 +0900

    setting clock

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-const images = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg"];
+const images = ["01.jpg", "02.jpg","03.jpg","04.jpg"];
 
 const chosenImg = images[Math.floor(Math.random() * images.length)];
 
@@ -8,5 +8,3 @@ background.src = `image/${chosenImg}`;
 const back = document.querySelector("#background")
 
 back.appendChild(background);
-back.style.backgroundSize = "cover";
-back.style.backgroundRepeat = "no-repeat";
