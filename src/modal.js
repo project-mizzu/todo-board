23337854573f9d220fa2a8460f368939bc40ca84
M006004 <76555348+M006004@users.noreply.github.com>
2021-11-28 16:31:03 +0900

    modal finished

--- src/modal.js
+++ src/modal.js
@@ -1,12 +1,33 @@
 const modal = document.querySelector("#modal");
+const loginInput = document.querySelector(".modal-input");
 const btn = document.querySelector(".modal-btn");
+const name = document.querySelector(".name-input");
 
-function modalDel(event) {
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
     event.preventDefault();
     modal.style.display = "none";
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-btn.addEventListener("click", modalDel);
+function paintGreetings(username) {
+    if (username === ''){
+      name.innerText = `Hello, Stranger!`;
+    } else {
+      name.innerText = `Good Day, ${username}`;
+    }
+}
 
-const modalValue = document.querySelector(".modal-input");
-const nameValue = document.querySelector(".name-input");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    modal.addEventListener("submit", onLoginSubmit);
+} else {
+    modal.style.display = "none";
+    paintGreetings(savedUsername);
+}
+
+btn.addEventListener("click", onLoginSubmit);
